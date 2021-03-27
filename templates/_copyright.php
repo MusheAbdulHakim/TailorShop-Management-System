c3cb7207a8e8f98db6b8758c91b0dcd21b2484e2
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-03-27 17:42:02 +0000

    profile link added

--- templates/_copyright.php
+++ templates/_copyright.php
@@ -1,6 +1,6 @@
 <!-- BEGIN: Footer-->
 <footer class="footer fixed-static footer-dark navbar-border navbar-shadow">
-		<p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">Copyright &copy; <?php echo Date('Y'); ?> <a class="text-bold-800 grey darken-2" href="#" target="_blank">Mushe Abdul-Hakim</a></span><span class="float-md-right d-none d-lg-block">Hand-crafted & Made with<i class="ft-heart pink"></i><span id="scroll-top"></span></span></p>
+		<p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">Copyright &copy; <?php echo Date('Y'); ?> <a class="text-bold-800 grey darken-2" href="https://github.com/MusheAbdulHakim" target="_blank">Mushe Abdul-Hakim</a></span><span class="float-md-right d-none d-lg-block">Hand-crafted & Made with<i class="ft-heart pink"></i><span id="scroll-top"></span></span></p>
 </footer>
 <!-- END: Footer-->
 <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery.min.js"></script>
