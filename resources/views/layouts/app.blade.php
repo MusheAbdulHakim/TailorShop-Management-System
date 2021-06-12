c3cf56b022009738a6e663fe761198522aa60908
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-06-12 23:42:14 +0000

    used @asset directive for links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,45 @@
 <!DOCTYPE html>
-<html
-    class="loading" lang="en" data-textdirection="ltr">
+<html class="loading" lang="en" data-textdirection="ltr">
     <!-- BEGIN: Head-->
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
         <title>{{ucfirst($title)}} - {{ucfirst(config('app.name'))}}</title>
-        <link rel="apple-touch-icon" href="app-assets/images/ico/apple-icon-120.png">
-        <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.ico">
+        <link rel="apple-touch-icon" href="{{asset('app-assets/images/ico/apple-icon-120.png')}}">
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('app-assets/fonts/material-icons/material-icons.min.css')}}">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700" rel="stylesheet">
         <link
-        rel="stylesheet" type="text/css" href="app-assets/fonts/material-icons/material-icons.min.css">
+        rel="stylesheet" type="text/css" href="{{asset('app-assets/fonts/material-icons/material-icons.min.css')}}">
 
         <!-- BEGIN: Vendor CSS-->
-        <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/material-vendors.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/extensions/toastr.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/tables/datatable/datatables.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/tables/extensions/buttons.dataTables.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/tables/datatable/buttons.bootstrap4.min.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/material-vendors.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/extensions/toastr.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/tables/datatable/datatables.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/tables/extensions/buttons.dataTables.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/tables/datatable/buttons.bootstrap4.min.css')}}">
         <!-- END: Vendor CSS-->
 
         <!-- BEGIN: Theme CSS-->
-        <link rel="stylesheet" type="text/css" href="app-assets/css/material.min.css">
-         <link rel="stylesheet" type="text/css" href="app-assets/css/components.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/css/bootstrap-extended.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/css/material-extended.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/css/material-colors.min.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/material.min.css')}}">
+         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/components.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/bootstrap-extended.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/material-extended.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/material-colors.min.css')}}">
         <!-- END: Theme CSS-->
 
         <!-- BEGIN: Page CSS-->
-        <link rel="stylesheet" type="text/css" href="app-assets/css/core/menu/menu-types/material-vertical-menu.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/css/plugins/animate/animate.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/css/plugins/extensions/toastr.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/fonts/simple-line-icons/style.min.css">
-        <link rel="stylesheet" type="text/css" href="app-assets/css/core/colors/palette-callout.min.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/core/menu/menu-types/material-vertical-menu.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/plugins/animate/animate.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/plugins/extensions/toastr.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/fonts/simple-line-icons/style.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/core/colors/palette-callout.min.css')}}">
 
         @stack('page-css')
         <!-- END: Page CSS-->
 
         <!-- BEGIN: Custom CSS-->
-        <link rel="stylesheet" type="text/css" href="assets/css/style.css"> <!-- END: Custom CSS-->
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}"> <!-- END: Custom CSS-->
 
     </head>
     <!-- END: Head-->
@@ -97,36 +96,35 @@
 
 
         <!-- BEGIN: Vendor JS-->
-        <script src="app-assets/vendors/js/material-vendors.min.js"> </script>
+        <script src="{{asset('app-assets/vendors/js/material-vendors.min.js')}}"> </script>
         <!-- BEGIN Vendor JS-->
 
         <!-- BEGIN: Page Vendor JS-->
-        <script src="app-assets/vendors/js/animation/jquery.appear.js"></script>
-        <script src="app-assets/vendors/js/tables/datatable/datatables.min.js"></script>
-        <script src="app-assets/vendors/js/tables/datatable/dataTables.buttons.min.js"></script>
-        <script src="app-assets/vendors/js/tables/datatable/buttons.bootstrap4.min.js"></script>
-        <script src="app-assets/vendors/js/tables/jszip.min.js"></script>
-        <script src="app-assets/vendors/js/tables/pdfmake.min.js"></script>
-        <script src="app-assets/vendors/js/tables/vfs_fonts.js"></script>
-        <script src="app-assets/vendors/js/tables/buttons.html5.min.js"></script>
-        <script src="app-assets/vendors/js/tables/buttons.print.min.js"></script>
-        <script src="app-assets/vendors/js/tables/buttons.colVis.min.js"></script>
-        <script src="app-assets/vendors/js/extensions/toastr.min.js"></script>
+        <script src="{{asset('app-assets/vendors/js/animation/jquery.appear.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/datatable/datatables.min.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/datatable/dataTables.buttons.min.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap4.min.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/jszip.min.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/pdfmake.min.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/vfs_fonts.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/buttons.html5.min.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/buttons.print.min.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/tables/buttons.colVis.min.js')}}"></script>
+        <script src="{{asset('app-assets/vendors/js/extensions/toastr.min.js')}}"></script>
         <!-- END: Page Vendor JS-->
 
         <!-- BEGIN: Theme JS-->
-        <script src="app-assets/js/core/app-menu.min.js"></script>
-        <script src="app-assets/js/core/app.min.js"></script>
-        <script src="app-assets/js/scripts/customizer.min.js"></script>
-        <script src="app-assets/js/scripts/footer.min.js"></script>
+        <script src="{{asset('app-assets/js/core/app-menu.min.js')}}"></script>
+        <script src="{{asset('app-assets/js/core/app.min.js')}}"></script>
+        <script src="{{asset('app-assets/js/scripts/customizer.min.js')}}"></script>
+        <script src="{{asset('app-assets/js/scripts/footer.min.js')}}"></script>
         <!-- END: Theme JS-->
 
         <!-- BEGIN: Page JS-->
-        <script src="app-assets/js/scripts/pages/material-app.min.js"></script>
-        <script src="app-assets/js/scripts/animation/animation.js"></script>
-        <script src="app-assets/js/scripts/pages/material-app.min.js"></script>
-	    <script src="app-assets/js/scripts/tables/datatables-extensions/datatable-button/datatable-html5.min.js"></script>
-        <script src="app-assets/js/scripts/extensions/toastr.min.js"></script>
+        <script src="{{asset('app-assets/js/scripts/animation/animation.js')}}"></script>
+        <script src="{{asset('app-assets/js/scripts/pages/material-app.min.js')}}"></script>
+	    <script src="{{asset('app-assets/js/scripts/tables/datatables-extensions/datatable-button/datatable-html5.min.js')}}"></script>
+        <script src="{{asset('app-assets/js/scripts/extensions/toastr.min.js')}}"></script>
         <script>
             @if(Session::has('message'))
             var type = "{{ Session::get('alert-type', '') }}";
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,28 +8,28 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 
 <title>{{ucfirst($title)}} - {{ucfirst(config('app.name'))}}</title>
-<link rel="apple-touch-icon" href="app-assets/images/ico/apple-icon-120.png">
-<link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.ico">
+<link rel="apple-touch-icon" href="{{asset('app-assets/images/ico/apple-icon-120.png')}}">
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('app-assets/images/ico/favicon.ico')}}">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="app-assets/fonts/material-icons/material-icons.min.css">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/fonts/material-icons/material-icons.min.css')}}">
 <!-- BEGIN: Vendor CSS-->
-<link rel="stylesheet" type="text/css" href="app-assets/vendors/css/material-vendors.min.css">
-<link rel="stylesheet" type="text/css" href="app-assets/vendors/css/forms/icheck/icheck.css">
-<link rel="stylesheet" type="text/css" href="app-assets/vendors/css/forms/icheck/custom.css">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/material-vendors.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/icheck/icheck.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/icheck/custom.css')}}">
 <!-- END: Vendor CSS-->
 <!-- BEGIN: Theme CSS-->
-<link rel="stylesheet" type="text/css" href="app-assets/css/material.min.css">
-<link rel="stylesheet" type="text/css" href="app-assets/css/components.min.css">
-<link rel="stylesheet" type="text/css" href="app-assets/css/bootstrap-extended.min.css">
-<link rel="stylesheet" type="text/css" href="app-assets/css/material-extended.min.css">
-<link rel="stylesheet" type="text/css" href="app-assets/css/material-colors.min.css">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/material.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/components.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/bootstrap-extended.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/material-extended.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/material-colors.min.css')}}">
 <!-- END: Theme CSS-->
 <!-- BEGIN: Page CSS-->
-<link rel="stylesheet" type="text/css" href="app-assets/css/core/menu/menu-types/material-vertical-menu.min.css">
-<link rel="stylesheet" type="text/css" href="app-assets/css/pages/login-register.min.css">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/core/menu/menu-types/material-vertical-menu.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/pages/login-register.min.css')}}">
 <!-- END: Page CSS-->
 <!-- BEGIN: Custom CSS-->
-<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
 <!-- END: Custom CSS-->
 </head>
 <!-- END: Head-->
@@ -49,7 +49,7 @@
                   <div class="card border-grey border-lighten-3 px-1 py-1 m-0">
                       <div class="card-header border-0">
                           <div class="card-title text-center">
-                              <img src="app-assets/images/logo/logo-dark.png" alt="logo">
+                              <img src="{{asset('app-assets/images/logo/logo-dark.png')}}" alt="logo">
                           </div>
                           <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2"><span>@yield('card-subtitle')</span></h6>
                       </div>
@@ -73,19 +73,19 @@
 </div>
 <!-- END: Content-->
 <!-- BEGIN: Vendor JS-->
-<script src="app-assets/vendors/js/material-vendors.min.js"></script>
+<script src="{{asset('app-assets/vendors/js/material-vendors.min.js')}}"></script>
 <!-- BEGIN Vendor JS-->
 <!-- BEGIN: Page Vendor JS-->
-<script src="app-assets/vendors/js/forms/validation/jqBootstrapValidation.js"></script>
-<script src="app-assets/vendors/js/forms/icheck/icheck.min.js"></script>
+<script src="{{asset('app-assets/vendors/js/forms/validation/jqBootstrapValidation.js')}}"></script>
+<script src="{{asset('app-assets/vendors/js/forms/icheck/icheck.min.js')}}"></script>
 <!-- END: Page Vendor JS-->
 <!-- BEGIN: Theme JS-->
-<script src="app-assets/js/core/app-menu.min.js"></script>
-<script src="app-assets/js/core/app.min.js"></script>
+<script src="{{asset('app-assets/js/core/app-menu.min.js')}}"></script>
+<script src="{{asset('app-assets/js/core/app.min.js')}}"></script>
 <!-- END: Theme JS-->
 <!-- BEGIN: Page JS-->
-<script src="app-assets/js/scripts/pages/material-app.min.js"></script>
-<script src="app-assets/js/scripts/forms/form-login-register.min.js"></script>
+<script src="{{asset('app-assets/js/scripts/pages/material-app.min.js')}}"></script>
+<script src="{{asset('app-assets/js/scripts/forms/form-login-register.min.js')}}"></script>
 <!-- END: Page JS-->
 </body>
 <!-- END: Body-->
