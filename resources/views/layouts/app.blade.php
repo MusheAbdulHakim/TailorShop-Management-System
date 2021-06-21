8e05b96988e3d88dac6bea7e6854b7c860bf92ce
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-06-21 00:25:25 +0000

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="loading" lang="en" data-textdirection="ltr">
+<html class="loading"  data-textdirection="ltr">
     <!-- BEGIN: Head-->
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
