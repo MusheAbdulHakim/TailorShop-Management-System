80195230d5e4e7217d22a37c97c7e2d19b315e91
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-06-21 00:25:14 +0000

    updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
 @push('page-css')
-	
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/material-vendors.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/weather-icons/climacons.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/fonts/meteocons/style.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/charts/morris.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/charts/chartist.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/charts/chartist-plugin-tooltip.css')}}">
+    <!-- END: Vendor CSS-->
+	<link rel="stylesheet" href="{{asset('app-assets/css/pages/dashboard-ecommerce.min.css')}}">
 @endpush
 
 @section('content')
@@ -87,9 +95,46 @@
 				</div>
 		</div>
 	</div>
+
 </div>
+<!-- Products sell and New Orders -->
+{{-- <div class="row match-height">
+    <div class="col-xl-12 col-12" id="ecommerceChartView">
+        <div class="card card-shadow">
+        <div class="card-header card-header-transparent py-20">
+            <div class="btn-group dropdown">
+            <a href="#" class="text-body dropdown-toggle blue-grey-700" data-toggle="dropdown">PRODUCTS SALES</a>
+            <div class="dropdown-menu animate" role="menu">
+                <a class="dropdown-item" href="#" role="menuitem">Sales</a>
+                <a class="dropdown-item" href="#" role="menuitem">Total sales</a>
+                <a class="dropdown-item" href="#" role="menuitem">profit</a>
+            </div>
+            </div>
+            <ul class="nav nav-pills nav-pills-rounded chart-action float-right btn-group" role="group">
+            <li class="nav-item"><a class="active nav-link" data-toggle="tab" href="#scoreLineToDay">Day</a></li>
+            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#scoreLineToWeek">Week</a></li>
+            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#scoreLineToMonth">Month</a></li>
+            </ul>
+        </div>
+        <div class="widget-content tab-content bg-white p-20">
+            <div class="ct-chart tab-pane active scoreLineShadow" id="scoreLineToDay"></div>
+            <div class="ct-chart tab-pane scoreLineShadow" id="scoreLineToWeek"></div>
+            <div class="ct-chart tab-pane scoreLineShadow" id="scoreLineToMonth"></div>
+        </div>
+        </div>
+    </div>
+    
+</div> --}}
+<!--/ Products sell and New Orders -->
 @endsection
 
 @push('page-js')
-	
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="{{asset('app-assets/vendors/js/charts/chartist.min.js')}}"></script>
+    <script src="{{asset('app-assets/vendors/js/charts/chartist-plugin-tooltip.min.js')}}"></script>
+    <script src="{{asset('app-assets/vendors/js/charts/raphael-min.js')}}"></script>
+    <script src="{{asset('app-assets/vendors/js/charts/morris.min.js')}}"></script>
+    <script src="{{asset('app-assets/vendors/js/timeline/horizontal-timeline.js')}}"></script>
+    <!-- END: Page Vendor JS-->
+	<script src="{{asset('app-assets/js/scripts/pages/dashboard-ecommerce.min.js')}}"></script>
 @endpush
